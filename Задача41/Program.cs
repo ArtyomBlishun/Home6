3b155df887c905c7efa29dec59dde14886f90103
Artyom Blishun <ablishun@mail.ru>
2022-12-20 18:55:35 +0300

    First commit

---
+++ Задача41/Program.cs
@@ -0,0 +1,25 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Write("Введите элементы: ");
+string inputNum = Console.ReadLine();
+int resoult = FillArray(inputNum);
+Console.WriteLine($"M элементов > 0: {resoult}");
+
+int FillArray(string num)
+{
+string[] arrayString = num.Split(" ");
+int[] arrayInt = Array.ConvertAll(arrayString, int.Parse);
+int count = 0;
+ 
+    for (int i = 0; i < arrayInt.Length; i++)
+    {
+        if (arrayInt[i] > 0)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+
+
